c6fbd6d35a6921be6a3df3e1879431f48b149959
Vasyl Obukh <vasiaobukh7@gmail.com>
2019-06-29 21:06:41 +0300

    wip: added api connection

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,27 @@
 const app = require('koa')();
 const router = require('./routers');
+const logger = require('koa-logger');
+const mongoose = require('mongoose');
+const koaRequest = require('koa-http-request');
+const { DB_NAME, PORT, API_BASE } = require('./config');
 
-const PORT = 8000;
+app.use(logger());
+app.use(function *(next) {
+  try {
+    yield next;
+  } catch (err) {
+    this.status = err.status || 500;
+    this.body = err.message;
+    this.app.emit('error', err, this);
+  }
+});
 
+app.use(koaRequest({
+  dataType: 'json',
+  timeout: 3000,
+  host: API_BASE
+}));
+
+mongoose.connect(`mongodb://localhost/${DB_NAME}`, { useNewUrlParser: true });
 app.use(router.routes());
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- src/server/routers.js
+++ src/server/routers.js
@@ -2,13 +2,17 @@ const router = require('koa-router')(
   {prefix: '/api'}
 );
 
-const mocha = [
-  {name: 'aaa', id: 1},
-  {name: 'bbb', id: 2}
-];
 
 router.get('/', function* (next) {
-  this.body = mocha;
+  const apiRes = [];
+  for (let i = 1; i <= 30; i++) {
+    apiRes[i-1] = yield this.get(`/pokemon/${i}`, null, {
+      'User-Agent': 'koa-http-request'
+    });
+  }
+
+  this.body = apiRes;
+  //this.body = [{a: 1}];
   yield next;
 });
 
